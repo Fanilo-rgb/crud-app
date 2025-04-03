7c0826e8e7cbf68ebb80b8457b7414bfc11fd9ed
Faniloniaina Princy <prxandxa@gmail.com>
2025-04-03 19:22:24 +0300

    Adding a navbar and a new page addTopic

---
+++ app/addTopic/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const AddTopic = () => {
+  return (
+    <form className="flex flex-col gap-3">
+      <input className="border border-slate-500 px-8 py-2" type="text" placeholder="Topic Title"/>
+    </form>
+  )
+}
+export default AddTopic
